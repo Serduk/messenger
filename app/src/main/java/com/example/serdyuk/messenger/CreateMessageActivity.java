a9f4a83a0c879271a01a5707150936ca2ec88271
Serdyuk <serduksergey90@gmail.com>
2017-11-28 18:10:06 +0200

    add text field

---
+++ app/src/main/java/com/example/serdyuk/messenger/CreateMessageActivity.java
@@ -0,0 +1,17 @@
+package com.example.serdyuk.messenger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class CreateMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+
+    public void onSendMessage(View view) {
+    }
+}
